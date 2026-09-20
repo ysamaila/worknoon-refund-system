b8b9095a3b41e39c73f058e313acdae8b04dc95b
yusuf-samaila <114921676+ysamaila@users.noreply.github.com>
2026-09-20 17:02:56 +0100

    fix: allow web dev server to use available port and allow 3001 in cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   // Configure CORS
   app.enableCors({
-    origin: [envService.frontendUrl, 'http://localhost:3000'],
+    origin: [envService.frontendUrl, 'http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   });
 
